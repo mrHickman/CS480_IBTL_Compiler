48f9ea72fc409c4476999f1633f2748f0f66ff31
Riley Glen Hickman <riley.g.hickman@gmail.com>
2014-01-26 00:20:05 -0800

    Test Files, Read From a File, and Token Class
    Added test files, that should be hand crafted with a test and expected
    to check against after, could be easy to automate later. Successfully
    read a full file with a buffer size less than and greater than the size
    of the file, from the scanner.py. Made a first attempt at a token class.

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/34/408c9648488600131f79dec8c3908a79
@@ -0,0 +1,8 @@
+'''
+Created on Jan 25, 2014
+
+@author: mr_hickman
+'''
+
+if __name__ == '__main__':
+    pass
--- CS480_Compiler/src/main.py
+++ CS480_Compiler/src/main.py
@@ -2,7 +2,42 @@
 Created on Jan 25, 2014
 
 @author: mr_hickman
-'''
 
+Description:
+    Hosts the main function for the compiler, requires an input file for the source code to be compiled. 
+    Currently in a state to only tokenize, all most.
+'''
+from optparse import OptionParser
+from scanner import Scanner
+
+def scan(srcFilePath):
+    myScanner = Scanner('TestFiles/test1')
+    while True:
+        char = myScanner.getNextCharacter()
+        if not char:        #This is the EOF character in python
+            break
+        else:
+            print char
+    
+    # myLexicalAnalyzer = lexicalAnalyzer(myScanner)
+    # myLexicalAnalyzer.tokenize()
+    
+
+def main():
+    parser = OptionParser("usage: %prog [options] arg1")
+    parser.add_option("-s", "--source", dest="SrcFilePath",
+                      type="string", default="",
+                      help="Input source file path")
+    (options, args) = parser.parse_args()
+    
+    if len(args) != 0:
+        parser.error("Incorrect number of arguments provided")
+    
+    SrcFilePath = options.SrcFilePath
+    scan(SrcFilePath)
+       
 if __name__ == '__main__':
-    pass
+    main()
+
+
+    
