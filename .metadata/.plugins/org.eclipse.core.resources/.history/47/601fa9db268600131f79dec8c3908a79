2186a3385795165489e57420fd085be21df6df45
Riley Glen Hickman <riley.g.hickman@gmail.com>
2014-01-25 20:41:56 -0800

    Psuedo Code for Scanner

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/47/601fa9db268600131f79dec8c3908a79
@@ -0,0 +1,28 @@
+'''
+Created on Jan 25, 2014
+
+@author: mr_hickman
+'''
+
+'''
+T           = 
+S           = 
+expr        = 
+oper        = 
+binops      = 
+unops       = 
+constants   = 
+strings     = ^"[a-zA-Z0-9]*"$|^true$|^false$
+name        = ^[a-zA-Z_][a-zA-Z0-9_]*$
+ints        = ^[0-9][0-9]*$
+floats      = ^[0-9][0-9]*.[0-9]*$|^[0-9][0-9]*e[0-9][0-9]*$
+stmts       = 
+printstmts  = 
+ifstmts     = 
+whilestmts  = 
+exprlist    = 
+letstmts    = 
+varlist     = 
+type        = 
+
+'''
--- CS480_Compiler/src/scanner.py
+++ CS480_Compiler/src/scanner.py
@@ -2,27 +2,46 @@
 Created on Jan 25, 2014
 
 @author: mr_hickman
+
+Description:
+    Facilitates file input massaging for lexical analyzer, by removing comments and duplicate spaces(' ' and '/t').
+    Note leaves '/n' characters for line position error reporting. 
+
 '''
 
 '''
-T           = 
-S           = 
-expr        = 
-oper        = 
-binops      = 
-unops       = 
-constants   = 
-strings     = ^"[a-zA-Z0-9]*"$|^true$|^false$
-name        = ^[a-zA-Z_][a-zA-Z0-9_]*$
-ints        = ^[0-9][0-9]*$
-floats      = ^[0-9][0-9]*.[0-9]*$|^[0-9][0-9]*e[0-9][0-9]*$
-stmts       = 
-printstmts  = 
-ifstmts     = 
-whilestmts  = 
-exprlist    = 
-letstmts    = 
-varlist     = 
-type        = 
+
 
 '''
+# File
+# ReadBuffer
+# CurrentPos
+# BufferSize == BUFFER_SIZE
+
+
+# get next character{
+    # If(ReadBuffer[currentpos] == '//')
+        # While(next ! /n)
+            # skip
+        # return /n
+    # If(IsMultiLineComment || (ReadBuffer[currentpos] == / && ReadBuffer[currentpos++] == *))
+        # While(next ! /n || (* && next /))
+            # skip
+        # If (* && next /)
+            # IsMultiLineComment = false
+            # skip through the */
+            # return next
+        # IsMultiLineComment = true
+        # return /n
+    # If(ReadBuffer[currentpos] == ' ' || /t )    
+        # While(next ' ' || '/t')
+            # skip, ++CurrentPos; # if at the end of a buffer then skip will remain in this loop for white space prior
+        # return ' '
+    # return ReadBuffer[CurrentPos++] # look for eof up one level
+# skip
+        # if(CurrentPos++ < BufferSize)
+                # return;
+        # else 
+            # fill buffer and set currentPos = 0
+# fill buffer
+             
